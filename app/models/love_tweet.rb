c4b2448c62cc4d73d4a0f417714649780b43037d
SiamKing <thesiameseelectric@gmail.com>
2017-09-08 09:02:15 -0700

    generated model for LoveTweet

---
+++ app/models/love_tweet.rb
@@ -0,0 +1,2 @@
+class LoveTweet < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170908155954) do
+ActiveRecord::Schema.define(version: 20170908160130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "love_tweets", force: :cascade do |t|
+    t.integer  "tweet_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "retweets", force: :cascade do |t|
     t.integer  "user_id"
